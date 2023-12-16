28e4fcc82f79d25e99757f8c5cd2c95518837270
Viktor Martynov <2013alfabravo@gmail.com>
2023-12-16 16:41:54 +0400

    project setup

---
+++ src/main/java/io/github/alfabravo2013/taskmanagement/TaskManagementSystemApplication.java
@@ -0,0 +1,12 @@
+package io.github.alfabravo2013.taskmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskManagementSystemApplication.class, args);
+    }
+}
---
+++ src/test/java/io/github/alfabravo2013/taskmanagement/TaskManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.alfabravo2013.taskmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
